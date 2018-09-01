aec8b9ab3010fd4cad8705fad18290a56298ca44
xiaomin1473 <964999271@qq.com>
2018-09-01 09:02:06 +0800

    行尾从CRLF改成了LF

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+# 编译+部署web站点
+
+# 需要配置如下参数
+# 项目路径，在Execute Shell中配置项目路径，pwd 就可以获得该项目路径
+# export PROJ_PATH=这个jenkins任务在部署机器上的路径
+
+# 输入你的环境上tomcat的全路径
+# export TOMCAT_APP_PATH=/var/www/
+
+### base 函数
 
 killTomcat()
 {
@@ -20,14 +31,17 @@ mvn clean install
 cd PROJ_PATH/h-parent/h-portal
 mvn clean install
 
+# 停tomcat
 killTomcat
 
+# 删除原有工程
 rm -rf TOMCAT_APP_PATH/ROOT
 rm -f TOMCAT_APP_PATH/ROOT.war
 rm -f TOMCAT_APP_PATH/h-web.war
 rm -f TOMCAT_APP_PATH/h-user.war
 rm -f TOMCAT_APP_PATH/h-portal.war
 
+# 复制新的工程
 cp PROJ_PATH/h-parent/h-web/target/h-web.war TOMCAT_APP_PATH/
 cp PROJ_PATH/h-parent/h-web/target/h-web1.war TOMCAT_APP_PATH/
 cp PROJ_PATH/h-parent/h-user/target/h-user.war TOMCAT_APP_PATH/
@@ -36,12 +50,7 @@ cp PROJ_PATH/h-parent/h-portal/target/h-portal.war TOMCAT_APP_PATH/
 cd TOMCAT_APP_PATH/
 mv h-web1.war ROOT.war
 
-# ??Tomcat
+# 启动Tomcat
 cd /opt/apache-tomcat-8.5.33
 
 sh bin/startup.sh
-
-
-
-
-
