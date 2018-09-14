39ecf256920cc3d87223ad3c19d176c14db15a47
xiaomin1473 <964999271@qq.com>
2018-09-14 13:16:41 +0800

    更新了首页AE动画

--- h-web/src/main/webapp/common/js/main.js
+++ h-web/src/main/webapp/common/js/main.js
@@ -1,15 +1,32 @@
 (function () {
     "use strict";
 
-    var navItems = $(".nav-wrapper").children("li");
+    
+    (function(){
+        var navItems = $(".nav-wrapper").children("li");
+        navItems.hover(function(){
+                this.className = "cur";
+                console.log(this);
+            }, 
+            function() {
+                this.className = "";
+            });
+    })();
 
-    navItems.hover(function(){
-        this.className = "cur";
-        console.log(this);
-    }, function() {
-        this.className = "";
-    });
-
-
-
+    (function(){
+        var contentItem = $('.content').children();
+        console.log(contentItem);
+        var i = 0;
+        var interText = setInterval(function() {
+            contentItem[i].className = "ae-riseIn ae-d3";
+            i++;
+            if(i === contentItem.length) {
+                clearInterval(interText);
+            }
+        }, 200);
+        
+    })();
+    
+    
+    
 })();
