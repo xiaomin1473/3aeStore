153ed110aeb83adf34395472aef2481d7fc9423c
xiaomin1473 <964999271@qq.com>
2018-09-25 19:49:29 +0800

    test

--- answer/src/main/java/answer/test.java
+++ answer/src/main/java/answer/test.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class test {
+public class test extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
