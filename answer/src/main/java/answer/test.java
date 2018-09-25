4616df045169c877538594d23bc57309234579e8
xiaomin1473 <964999271@qq.com>
2018-09-25 19:46:07 +0800

    test

--- answer/src/main/java/answer/test.java
+++ answer/src/main/java/answer/test.java
@@ -1,12 +1,23 @@
 package answer;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
 public class test {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("处理GET请求…………");
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html;charset=UTF-8");
+		out.println("<html><body><i>This is a GET request!</i><br><h1>hello servlet</h1></body></html>");
+	}
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		System.out.println("处理POST请求…………");
