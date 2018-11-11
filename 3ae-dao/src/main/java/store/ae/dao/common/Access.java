2f8ecce9933eb325b35191591671c31c9070058a
xiaomin1473 <964999271@qq.com>
2018-11-11 15:21:06 +0800

    change router

--- 3ae-dao/src/main/java/store/ae/dao/common/Access.java
+++ 3ae-dao/src/main/java/store/ae/dao/common/Access.java
@@ -11,7 +11,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 public class Access {
 	public SqlSession getSqlSession() throws IOException {
 		// 通过配置文件获取数据库链接信息
-		Reader reader = Resources.getResourceAsReader("com/ae/dao/init/config.xml");
+		Reader reader = Resources.getResourceAsReader("store/ae/dao/init/config.xml");
 		
 		// 通过配置文件构建一个SqlSessionFactory
 		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
--- 3ae-dao/src/main/java/store/ae/dao/init/MessageDao.java
+++ 3ae-dao/src/main/java/store/ae/dao/init/MessageDao.java
@@ -16,33 +16,22 @@ public class MessageDao {
 		SqlSession sqlSession = null;
 		List<Message> messageList = new ArrayList<Message>();
 
-		
+
 		try {
-			System.out.println("this is try");
 			sqlSession = dbAccess.getSqlSession();
-			
+
 			messageList = sqlSession.selectList("Message.queryMessageList");
 		} catch (IOException e) {
 			// TODO: handle exception
 			e.printStackTrace();
-			System.out.println("this is catch");
 		} finally {
-			System.out.println("this is finally");
 			if(sqlSession != null) {
-				System.out.println("this is finally close");
 				sqlSession.close();
 			} else {
 				System.out.println("sqlSession is null");
 			}
-			
 		}
-		
-		return messageList;
-	}
-	
-	public static void main(String[] args) throws SQLException {
-		MessageDao initDBae = new MessageDao();
 
-		initDBae.queryMessageList("", "");
+		return messageList;
 	}
 }
