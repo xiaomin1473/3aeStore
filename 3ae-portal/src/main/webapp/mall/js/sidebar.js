bc402584722227be4df71ab41a56e0f4ae939a50
xiaomin1473 <964999271@qq.com>
2018-11-21 15:02:39 +0800

    ae_mall_index

--- 3ae-portal/src/main/webapp/mall/js/sidebar.js
+++ 3ae-portal/src/main/webapp/mall/js/sidebar.js
@@ -8,63 +8,72 @@
         return false;
     });
 
-    $(window).scroll(function(){
-        var scrollHeight = $(this).scrollTop();
-        if(scrollHeight > 100){
-            $('.Rtop').removeClass('dn');
-            $('.tool-line:last').removeClass('dn');
-        }else{
-            $('.Rtop').addClass('dn');
-            $('.tool-line:last').addClass('dn');
+    var index;
+    //绑定字段id
+    var elevator = $("#elevator");
+    var floors = $(".floor");
+    var li = elevator.find("li");
+    
+    //自身宽高
+    var elevatorHeight = elevator.height();
+    var elevatorWidth = elevator.width();
+
+    //页面宽高
+    var winHeight = $(window).height();
+    var winWidth = $(window).width();
+
+    //计算位置
+    var top = (winHeight-elevatorHeight)/2;
+    var left = (winWidth-1200)/2-elevatorWidth - 5;
+    
+    elevator.css({"position":"fixed","top":top,"left":left});
+    $(window).resize(function(){
+		winWidth = $(this).width();
+		left = (winWidth-1200)/2-elevatorWidth - 5;
+		elevator.css({"left":left});
+	});
+    
+    //计算楼层高度
+    var floorTop = $(".floor").offset().top - top;
+
+    //点击scroll
+    li.click(function(){
+		index = $(this).index();
+		top = parseInt(floors.eq(index).offset().top-60);
+		$("body,html").stop().animate({scrollTop:top});
+    });
+    
+    //监听scroll,左侧导航改变样式
+	$(window).scroll(function() {
+		var scrollTop = $(document).scrollTop();
+        var guessloveTop = $(".brand-list").offset().top;
+        var content = $(".brand-wrap").offset().top;
+
+		if (scrollTop>floorTop) {
+			elevator.stop().animate({"opacity":1});
+			elevator.css({'*display':'block','display':'block'});
+		} else{
+			elevator.stop().animate({"opacity":0});
+			elevator.css({'*display':'none','display':'none'});
+		}
+		
+		if (scrollTop>guessloveTop-600) {
+			elevator.stop().animate({"opacity":0});
+			elevator.css({'*display':'none','display':'none'});
+		}
+		
+		for (var i=0;i<floors.length;i++) {
+			top =  parseInt(floors.eq(i).offset().top);
+			if(scrollTop >= top-500){
+				li.eq(i).addClass("active").siblings().removeClass("active");
+			}
         }
-    });
-
-
-    $('.tel').hover(function () {
-        $('.tel img').attr("src", "img/tel_h.png");
-        $('.hidetelBox').css("display","block");
-    }, function () {
-        $('.tel img').attr("src", "img/tel.png");
-        $('.hidetelBox').css("display","none");
-    });
-
-    $('.mail').hover(function () {
-        $('.mail img').attr("src", "img/mail_h.png");
-        $('.hidemailBox').css("display","block");
-    }, function () {
-        $('.mail img').attr("src", "img/mail.png");
-        $('.hidemailBox').css("display","none");
-    });
-
-    $('.free').hover(function () {
-        $('.free img').attr("src", "img/free_h.png");
-        $('.hidefreeBox').css("display","block");
-    }, function () {
-        $('.free img').attr("src", "img/free.png");
-        $('.hidefreeBox').css("display","none");
-    });
-
-    $('.question').hover(function () {
-        $('.question img').attr("src", "img/question_h.png");
-        $('.hidequestionBox').css("display","block");
-    }, function () {
-        $('.question img').attr("src", "img/question.png");
-        $('.hidequestionBox').css("display","none");
-    });
-
-    $('.tmairuan').hover(function () {
-        $('.tmairuan img').eq(0).attr("src", "img/mairuan_h.png");
-        $('.hidetmairuanBox').css("display","block");
-    }, function () {
-        $('.tmairuan img').eq(0).attr("src", "img/mairuan.png");
-        $('.hidetmairuanBox').css("display","none");
-    });
-
-    $('.Rtop').hover(function () {
-        $('.Rtop img').attr("src", "img/top_h.png");
-        $('.hideRtop').css("display","block");
-    }, function () {
-        $('.Rtop img').attr("src", "img/top.png");
-        $('.hideRtop').css("display","none");
+        
+        //顶部导航
+        if (scrollTop>content) {
+            $(".scroll-navbar").addClass("show");
+        } else{
+            $(".scroll-navbar").removeClass("show");
+        }
     });
 })();
