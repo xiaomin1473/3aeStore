7fa0baef46d7979b16152fb1d0b0f40dd890e16d
xiaomin1473 <964999271@qq.com>
2018-12-03 17:09:31 +0800

    Optimizing database tables

--- 3ae-dao/src/main/resources/store/ae/dao/mall/sql/goods.sql
+++ 3ae-dao/src/main/resources/store/ae/dao/mall/sql/goods.sql
@@ -3,25 +3,28 @@ use store_3ae_mall;
 
 CREATE TABLE goods (
 `goods_id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品ID',
-`goods_type_id` bigint NOT NULL COMMENT '商品类型ID',
+`class` bigint NOT NULL COMMENT '商品类别',
 `name` varchar(120) NOT NULL COMMENT '商品名称',
 `number` int NOT NULL COMMENT '库存数量',
+`unit` varchar(3) NOT NULL COMMENT '商品单位',
 `price` decimal NOT NULL COMMENT '商品价格',
+`img_url` varchar(120) NOT NULL COMMENT '商品图片',
 `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
 `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
 PRIMARY KEY (goods_id),
-key idx_goods_type_id(goods_type_id),
+key idx_class(class),
 key idx_price(price),
 key idx_create_time(gmt_create)
 )ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='商品表';
 
 -- 初始化数据
-insert into goods(goods_type_id, name, number, price)
+insert into goods(class, name, number, unit, price, img_url)
 values
-	(1000, 'iphone6 plus', 100, 4000);
+	(10111100, 'iphone6 plus', 100, '部', 4000, './assests/img/iphone.png;');
 	
 CREATE TABLE goods_image (
 `goods_id` bigint NOT NULL COMMENT '商品ID',
+`class` bigint NOT NULL COMMENT '商品类别',
 `name` varchar(120) NOT NULL COMMENT '图片名称',
 `number_info` int NOT NULL COMMENT '图片数量',
 `number_detail` int NOT NULL COMMENT '详情图片数量',
@@ -31,53 +34,59 @@ CREATE TABLE goods_image (
 `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
 `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
 PRIMARY KEY (goods_id),
+key idx_class(class),
 key idx_create_time(gmt_create)
 )ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='商品图片表';
 
 -- 初始化数据
-insert into goods_image(goods_id, name, number_info, number_detail, suffix_type, urls_info, urls_detail)
+insert into goods_image(goods_id, class, name, number_info, number_detail, suffix_type, urls_info, urls_detail)
 values
-	(1000, 'iphone6 plus', 1, 1, 'png', './assests/img/iphone.png;', './assests/img/iphone.png;');
+	(1000, 10111100, 'iphone6 plus', 1, 1, 'png', './assests/img/iphone.png;', './assests/img/iphone.png;');
 	
 CREATE TABLE goods_type (
 `goods_type_id` bigint NOT NULL AUTO_INCREMENT COMMENT '商品类型ID',
 `name` varchar(120) NOT NULL COMMENT '类型名称',
-`grade` bigint NOT NULL COMMENT '类型级别',
-`parent_grade` bigint NOT NULL COMMENT '父类型级别',
+`class` bigint NOT NULL COMMENT '分类',
+`grade` bigint NOT NULL COMMENT '分类等级',
+`series` bigint NOT NULL COMMENT '等级系列',
 `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
 `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
 PRIMARY KEY (goods_type_id),
+key idx_class(class),
 key idx_grade(grade),
-key idx_parent_grade(parent_grade),
+key idx_series(series),
 key idx_create_time(gmt_create)
 )ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='商品类型表';
 
 -- 初始化数据
-insert into goods_type(name, grade, parent_grade)
+insert into goods_type(name, class, grade, series)
 values
-	('手机', 10001100, 10001000),
-	('电子', 10001000, 10000000),
-	('数码', 10001200, 10001000),
-	('相机', 10001300, 10001000);
+	('手机', 10111100, 10111000, 10100000),
+	('电子', 10111000, 10110000, 10100000),
+	('数码', 10111200, 10111000, 10100000),
+	('相机', 10111300, 10111000, 10100000);
 
 CREATE TABLE goods_detail (
 `goods_id` bigint NOT NULL COMMENT '商品ID',
+`class` bigint NOT NULL COMMENT '分类',
 `name` varchar(120) NOT NULL COMMENT '商品名称',
 `detail` text(300) NOT NULL COMMENT '商品详情',
 `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
 `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
 PRIMARY KEY (goods_id),
+key idx_class(class),
 key idx_create_time(gmt_create)
 )ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='商品详情表';
 
 -- 初始化数据
-insert into goods_detail(goods_id, name, detail)
+insert into goods_detail(goods_id, class, name, detail)
 values
-	(1000, 'iphone6 plus', '暂无');
+	(1000, 10111300, 'iphone6 plus', '暂无');
 	
 CREATE TABLE goods_evaluate (
 `evaluate_id` bigint NOT NULL AUTO_INCREMENT COMMENT '评价ID',
 `goods_id` bigint NOT NULL COMMENT '商品ID',
+`class` bigint NOT NULL COMMENT '分类',
 `name` varchar(120) NOT NULL COMMENT '评价名称',
 `evaluate_rank` int NOT NULL COMMENT '评价等级',
 `user_id` bigint NOT NULL COMMENT '评价用户',
@@ -86,16 +95,17 @@ CREATE TABLE goods_evaluate (
 `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
 PRIMARY KEY (evaluate_id),
 key idx_goods_id(goods_id),
+key idx_class(class),
 key idx_evaluate_rank(evaluate_rank),
 key idx_user_id(user_id),
 key idx_create_time(gmt_create)
 )ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='用户评价表';
 
 -- 初始化数据
-insert into goods_evaluate(goods_id, name, evaluate_rank, user_id, content)
+insert into goods_evaluate(goods_id, class, name, evaluate_rank, user_id, content)
 values
-	(1000, '默认评价', 10, 1000, "五星好评!"),
-	(1000, '默认评价', 9, 1001, "好评!");
+	(1000, 10111300, '默认评价', 10, 1000, "五星好评!"),
+	(1000, 10111300, '默认评价', 9, 1001, "好评!");
 
 
 
--- 3ae-dao/src/main/resources/store/ae/dao/mall/sql/order.sql
+++ 3ae-dao/src/main/resources/store/ae/dao/mall/sql/order.sql
@@ -5,40 +5,27 @@ CREATE TABLE orders (
 `order_id` bigint NOT NULL AUTO_INCREMENT COMMENT '订单ID',
 `user_id` bigint NOT NULL COMMENT '用户ID',
 `goods_id` bigint NOT NULL COMMENT '商品ID',
+`class` bigint NOT NULL COMMENT '分类',
+`name` varchar(120) NOT NULL COMMENT '商品名称',
 `goods_number` int NOT NULL COMMENT '购买数量',
-`order_status` int NOT NULL COMMENT '订单状态',
-`deliver_id` bigint NOT NULL COMMENT '收货ID',
-`invoice` int NOT NULL COMMENT '发票类型',
+`unit` varchar(3) NOT NULL COMMENT '单位',
+`price` decimal NOT NULL COMMENT '订单价格',
+`img_url` varchar(120) NOT NULL COMMENT '商品图片',
+`status` tinyint NOT NULL DEFAULT -1 COMMENT '-1无效 0成功 1已付款 2已发货 3已收货 4已评价 5已退货 6已退款',
+`user_name` varchar(120) NOT NULL COMMENT '收货联系人姓名',
+`user_phone` bigint NOT NULL COMMENT '收货联系人手机号',
+`address` varchar(120) NOT NULL COMMENT '收货地址',
+`invoice` tinyint NOT NULL DEFAULT -1 COMMENT '发票类型 -1无效 0默认',
 `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
 `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
 PRIMARY KEY (order_id),
 key idx_user_id(user_id),
 key idx_goods_id(goods_id),
-key idx_deliver_id(deliver_id),
+key idx_class(class),
 key idx_create_time(gmt_create)
 )ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='订单表';
 
 -- 初始化数据
-insert into orders(user_id, goods_id, goods_number, order_status, deliver_id, invoice)
+insert into orders(user_id, goods_id, class, name, goods_number, unit, price, img_url, user_name, user_phone, address)
 values
-	(1000, 1000, 1, 4, 1000, 0);
-
-CREATE TABLE user_deliver (
-`deliver_id` bigint NOT NULL AUTO_INCREMENT COMMENT '收货ID',
-`user_id` bigint NOT NULL COMMENT '用户ID',
-`name` varchar(120) NOT NULL COMMENT '收货联系人姓名',
-`phone` bigint NOT NULL COMMENT '收货联系人手机号',
-`address` varchar(120) NOT NULL COMMENT '收货地址',
-`option_name` varchar(120) NOT NULL COMMENT '备选人姓名',
-`option_phone` bigint NOT NULL COMMENT '备选人手机号',
-`gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-`gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-PRIMARY KEY (deliver_id),
-key idx_user_id(user_id),
-key idx_create_time(gmt_create)
-)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='收货详情表';
-
--- 初始化数据
-insert into user_deliver(deliver_id, user_id, name, phone, address, option_name, option_phone)
-values
-	(1000, 1000, "赵宇明", 18235010340, "天堂101", "暂无", 0);
+	(1000, 1000, 10111300,'iphone6 plus', 1, '部', 1000, './assests/img/iphone.png;', "赵宇明", 18235010340, "天上人间");
