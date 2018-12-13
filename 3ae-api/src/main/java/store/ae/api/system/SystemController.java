04de79ce25c77afa07bb3680fc6618cfbae2fafe
xiaomin1473 <964999271@qq.com>
2018-12-13 15:30:03 +0800

    split system apiService

---
+++ 3ae-api/src/main/java/store/ae/api/system/SystemController.java
@@ -0,0 +1,25 @@
+package store.ae.api.system;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import store.ae.dto.service.mall.feast.SeckillResult;
+
+
+@Controller
+@RequestMapping("/")
+public class SystemController {
+	
+
+	@RequestMapping(value="/time/now", method = RequestMethod.GET)
+	@ResponseBody
+	public SeckillResult<Long> time() {
+		Date now = new Date();
+		
+		return new SeckillResult<Long>(true, now.getTime());
+	}
+}
