22c6a415ecfc232d49fd4fec69aa18eb9652855e
xiaomin1473 <964999271@qq.com>
2018-12-29 16:25:35 +0800

    deploy-jenkis.sh

--- build.bat
+++ build.bat
@@ -1,5 +1,20 @@
 git add * 
-git commit -m "docker"
+git commit -m "deploy-jenkis.sh"
 git push
 
 mvn install
+
+xcopy /s /e /y 3ae-server\target\server.jar app\server
+xcopy /s /e /y 3ae-api\target\api.war app\api
+xcopy /s /e /y 3ae-answer\target\answer.war app\answer
+
+xcopy /s /e /y 3ae-api\target\api app\api\api
+xcopy /s /e /y 3ae-answer\target\answer app\answer\answer
+
+xcopy /s /e /y 3ae-admin\target\admin.war app\main\admin
+xcopy /s /e /y 3ae-portal\target\portal.war app\main\custom\portal
+xcopy /s /e /y 3ae-user\target\user.war app\main\custom\user
+
+xcopy /s /e /y 3ae-admin\target\admin app\main\admin\admin
+xcopy /s /e /y 3ae-portal\target\portal app\main\custom\portal\portal
+xcopy /s /e /y 3ae-user\target\user app\main\custom\user\user
---
+++ deploy-jenkins.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+# 编译+部署web站点
+
+# 需要配置如下参数
+# 项目路径，在Execute Shell中配置项目路径，pwd 就可以获得该项目路径
+# export PROJ_PATH=这个jenkins任务在部署机器上的路径
+
+# 输入你的环境上tomcat的全路径
+# export TOMCAT_APP_PATH=/var/www/
+
+# 输入你的环境上netty的全路径
+# export NETTY_APP_PATH=/var/netty/
+
+### base 函数
+
+killTomcat()
+{
+	pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
+	echo "tomcat ID list :$pid"
+	if [ "$pid" -le 0 ]
+	then
+		echo "no tomcat pid alive"
+	else
+		kill -9 $pid
+	fi
+}
+
+killNetty()
+{
+	pid=`ps -ef|grep netty|grep -v grep java|awk '{print $2}'`
+	echo "netty ID list :$pid"
+	if [ "$pid" -le 0 ]
+	then
+		echo "no netty pid alive"
+	else
+		kill -9 $pid
+	fi
+}
+
+cd $PROJ_PATH/3aeStore
+mvn clean install
+
+# 停tomcat
+killTomcat
+
+# 停netty
+killNetty
+
+
+# 删除原有工程
+# rm -rf $TOMCAT_APP_PATH/app/agent/agent
+rm -rf $TOMCAT_APP_PATH/app/answer/answer.war
+rm -rf $TOMCAT_APP_PATH/app/api/api.war
+# rm -rf $TOMCAT_APP_PATH/app/agent/agent
+rm -rf $TOMCAT_APP_PATH/app/answer/answer
+rm -rf $TOMCAT_APP_PATH/app/api/api
+# rm -rf $TOMCAT_APP_PATH/app/ROOT
+
+rm -rf $TOMCAT_APP_PATH/app/test/custom/portal/ROOT.war
+rm -rf $TOMCAT_APP_PATH/app/test/custom/user/ROOT.war
+rm -rf $TOMCAT_APP_PATH/app/test/admin/admin.war
+rm -rf $TOMCAT_APP_PATH/app/test/custom/portal/ROOT
+rm -rf $TOMCAT_APP_PATH/app/test/custom/user/ROOT
+rm -rf $TOMCAT_APP_PATH/app/test/admin/admin
+
+rm -rf $TOMCAT_APP_PATH/app/main/custom/portal/ROOT.war
+rm -rf $TOMCAT_APP_PATH/app/main/custom/user/ROOT.war
+rm -rf $TOMCAT_APP_PATH/app/main/admin/admin.war
+rm -rf $TOMCAT_APP_PATH/app/main/custom/portal/ROOT
+rm -rf $TOMCAT_APP_PATH/app/main/custom/user/ROOT
+rm -rf $TOMCAT_APP_PATH/app/main/admin/admin
+
+##
+# 复制新的工程
+cp $PROJ_PATH/3aeStore/3ae-admin/target/3ae-admin.war $TOMCAT_APP_PATH/app/main/admin/admin.war
+cp $PROJ_PATH/3aeStore/3ae-user/target/3ae-user.war $TOMCAT_APP_PATH/app/main/custom/user/ROOT.war
+cp $PROJ_PATH/3aeStore/3ae-portal/target/3ae-portal.war $TOMCAT_APP_PATH/app/main/custom/portal/ROOT.war
+
+cp -r $PROJ_PATH/3aeStore/3ae-admin/target/3ae-admin $TOMCAT_APP_PATH/app/main/admin/admin
+cp -r $PROJ_PATH/3aeStore/3ae-user/target/3ae-user $TOMCAT_APP_PATH/app/main/custom/user/ROOT
+cp -r $PROJ_PATH/3aeStore/3ae-portal/target/3ae-portal $TOMCAT_APP_PATH/app/main/custom/portal/ROOT
+
+cp $PROJ_PATH/3aeStore/3ae-admin/target/3ae-admin.war $TOMCAT_APP_PATH/app/test/admin/admin.war
+cp $PROJ_PATH/3aeStore/3ae-user/target/3ae-user.war $TOMCAT_APP_PATH/app/test/custom/user/ROOT.war
+cp $PROJ_PATH/3aeStore/3ae-portal/target/3ae-portal.war $TOMCAT_APP_PATH/app/test/custom/portal/ROOT.war
+
+cp -r $PROJ_PATH/3aeStore/3ae-admin/target/3ae-admin $TOMCAT_APP_PATH/app/test/admin/admin
+cp -r $PROJ_PATH/3aeStore/3ae-user/target/3ae-user $TOMCAT_APP_PATH/app/test/custom/user/ROOT
+cp -r $PROJ_PATH/3aeStore/3ae-portal/target/3ae-portal $TOMCAT_APP_PATH/app/test/custom/portal/ROOT
+
+cp $PROJ_PATH/3aeStore/3ae-api/target/3ae-api.war $TOMCAT_APP_PATH/app/api/api.war
+cp $PROJ_PATH/3aeStore/3ae-answer/target/3ae-answer.war $TOMCAT_APP_PATH/app/answer/answer.war
+
+cp -r $PROJ_PATH/3aeStore/3ae-api/target/3ae-api $TOMCAT_APP_PATH/app/api/api
+cp -r $PROJ_PATH/3aeStore/3ae-answer/target/3ae-answer $TOMCAT_APP_PATH/app/answer/answer
+# cp $PROJ_PATH/3aeStore/3ae-agent/target/3ae-agent-jar-with-dependencies.jar $NETTY_APP_PATH/www/app/agent/agent
+# cp $PROJ_PATH/3aeStore/3ae-server/target/3ae-server-jar-with-dependencies.jar $NETTY_APP_PATH/server/server.jar
+
+
+# 启动Tomcat
+sh /opt/apache-tomcat-8.5.35/bin/startup.sh
+
+echo "this is lastest version"
+
+startMysqld()
+{
+	pid=`ps -ef|grep mysqld|grep mysqld|awk '{print $2}'`
+	echo "mysql ID list :$pid"
+	if [ "$pid" -le 0 ]
+	then
+		service mysqld start
+	else
+		echo "mysqld is starting"
+	fi
+}
+
+startMysqld
