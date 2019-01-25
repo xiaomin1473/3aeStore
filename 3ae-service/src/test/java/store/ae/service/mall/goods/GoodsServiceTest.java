7d2e278eca1bef37369daf239b1e60716fa5d322
xiaomin147 <964999271@qq.com>
2019-01-25 14:52:12 +0800

    Add redis cache data

--- 3ae-service/src/test/java/store/ae/service/mall/goods/GoodsServiceTest.java
+++ 3ae-service/src/test/java/store/ae/service/mall/goods/GoodsServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -27,9 +26,6 @@ public class GoodsServiceTest {
 	
 	@Autowired
 	private GoodsService goodsService;
-	
-	@Autowired
-    private RedisTemplate<String, Object> redisTemplate;
 
 	@Test
 	public void testGetCategoryList() {
