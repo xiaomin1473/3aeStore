d8c9f0369379b73dd3acac18a328ac54a0140011
xiaomin1473 <505040575@qq.com>
2019-03-19 14:10:51 +0800

    change blog

--- 3ae-portal/src/main/webapp/assets/js/main.js
+++ 3ae-portal/src/main/webapp/assets/js/main.js
@@ -17,13 +17,14 @@
         var contentItem = $('.content').children();
         console.log(contentItem);
         var i = 0;
-        var interText = setInterval(function() {
-            contentItem[i].className = "ae-riseIn ae-d3";
-            i++;
-            if(i === contentItem.length) {
-                clearInterval(interText);
-            }
-        }, 100);
+        // 给标签添加自动进入动画
+        // var interText = setInterval(function() {
+        //     contentItem[i].className = "ae-riseIn ae-d3";
+        //     i++;
+        //     if(i === contentItem.length) {
+        //         clearInterval(interText);
+        //     }
+        // }, 100);
 
         var bgMusic = document.getElementById("music");
         var status = false;
