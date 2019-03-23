e32a6f31c605ab69de4bd0e823137917660cf3bc
xiaomin1473 <505040575@qq.com>
2019-03-23 08:32:01 +0800

    optimize web frontend architecture

--- 3ae-flow/src/main/webapp/config/config.js
+++ 3ae-flow/src/main/webapp/config/config.js
@@ -1,6 +1,6 @@
 Config = {
     alias: {
-        htmlPath: "/docker/views/component/filler/",
+        htmlPath: "/docker/views/component/html/",
         jsPath: "/docker/views/component/script/",
         cssPath: "/docker/views/component/style/",
         pagePath: "/docker/views/pages/",
