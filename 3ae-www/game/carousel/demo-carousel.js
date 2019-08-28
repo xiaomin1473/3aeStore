dcaff946e2b964b041fccb297d38e43bc38d4200
xiaomin1473 <505040575@qq.com>
2019-08-28 16:02:31 +0800

    add pattern chapter7

--- 3ae-www/game/carousel/demo-carousel.js
+++ 3ae-www/game/carousel/demo-carousel.js
@@ -17,11 +17,33 @@ log.info("log init");
 ** 
 ** 
 */
+
+function protect() {
+    that = arguments[0];
+    var startX, startY, moveEndX, moveEndY, X, Y;
+
+    document.addEventListener("touchstart", function(e) {
+        startX = e.changedTouches[0].pageX,
+        startY = e.changedTouches[0].pageY;
+    });
+    document.addEventListener("touchmove", function(e) {
+        moveEndX = e.changedTouches[0].pageX,
+        moveEndY = e.changedTouches[0].pageY,
+        X = moveEndX - startX,
+        Y = moveEndY - startY;　
+        if (Math.abs(X) > Math.abs(Y) && Math.abs(X) > 20) {
+            that.operate = true;
+        }　
+    });
+    document.addEventListener("touchend", function() {
+    });
+}
+
 var LoopImages = (function(){
     var carousel = function(container, imgArr) {
         var wrapper = document.createElement('div');
         wrapper.style.marginLeft = -container.clientWidth + "px";
-        wrapper.style.transition = ".3s all linear";
+        wrapper.style.transition = ".25s all ease-out";
         imgArr.push(imgArr[0]);
         imgArr.unshift(imgArr[imgArr.length-2]);
 
@@ -42,6 +64,7 @@ var LoopImages = (function(){
         this.arrow = arrow;             // 箭头
         this.interval = null;           // 定时器
         this.curr = 1;                  // 当前图片
+        this.operate = false;           // 操作保护
     }
 
     _loopImages.prototype = {
@@ -78,44 +101,41 @@ SlideLoopImage.prototype.init = function() {
     wrapper.addEventListener("mouseleave", function(e) {
         that.animate();
     })
-
-    
     this.addTouch(wrapper);
+    protect(this);
 }
-
-
 
 SlideLoopImage.prototype.changeImage = function() {
     var wrapper = this.container.childNodes[0];
-    that = this;
     if("pre" == arguments[0]) {
-        // 判断临界值,并添加动画
-        if (this.curr == 0) {
-            this.curr = this.imagesArray.length - 2;
-            wrapper.style.transition = ".3s all linear";
+        // 判断临界值,并清除动画
+        if (this.curr == 1) {
+            this.curr = this.imagesArray.length - 1;
+            if(wrapper.style.marginLeft == "-320px") {
+                // 初始向左时，解决bug
+                wrapper.style.marginLeft = -(this.container.clientWidth * this.curr) + 'px';
+            } else {
+                wrapper.style.marginLeft = -(this.container.clientWidth * (this.curr-1)) +
+                (parseInt(wrapper.style.marginLeft.split('px')[0])) + 'px';
+            }
+            wrapper.style.transitionProperty="none";
         }
         // 根据当前节点位置curr来改变wrapper的margin-left，动画transiton实现
         wrapper.style.marginLeft = -(this.container.clientWidth * --this.curr) + 'px';
-        // 清除动画
-        setTimeout(function(){
-            if(that.curr == 0) {
-                wrapper.style.transition = "0s";
-                wrapper.style.marginLeft = -(that.container.clientWidth * (that.imagesArray.length-2)) + 'px';
-        }}, 900);
+        // 添加动画
+        wrapper.style.transitionProperty="all";
     } else if("next" == arguments[0]) {
         // 见pre
-        if (this.curr == (this.imagesArray.length-1)) {
-            this.curr = 1;
-            wrapper.style.transition = ".3s all linear";
+        if (this.curr == (this.imagesArray.length-2)) {
+            wrapper.style.marginLeft = (parseInt(wrapper.style.marginLeft.split('px')[0]) + 
+                                    (this.container.clientWidth * this.curr)) + 'px';
+            this.curr = 0;
+            wrapper.style.transitionProperty="none";
         }
         wrapper.style.marginLeft = -(this.container.clientWidth * ++this.curr) + 'px';
-        setTimeout(function(){
-            if(that.curr == (that.imagesArray.length-1)) {
-                wrapper.style.transition = "0s";
-                wrapper.style.marginLeft = -(that.container.clientWidth * 1) + 'px';
-        }}, 900);
+        wrapper.style.transitionProperty="all";
     } else {
-        log.info("no change function");
+        wrapper.style.marginLeft = -(this.container.clientWidth * this.curr) + 'px';
     }
 }
 
@@ -127,27 +147,44 @@ SlideLoopImage.prototype.animate = function() {
 }
 
 SlideLoopImage.prototype.addTouch = function(wrapper) {
-    var startX, startY, moveEndX, moveEndY, X, Y;
+    var startX, startY, moveEndX, moveEndY, lastMoveX, lastMoveY, X, Y;
     that = this;
-
     wrapper.addEventListener("touchstart", function(e) {
         clearInterval(that.interval);
-　　　　startX = e.changedTouches[0].pageX,
-　　　　startY = e.changedTouches[0].pageY;
-        
-　　});
-　　wrapper.addEventListener("touchmove", function(e) {
-　　　　moveEndX = e.changedTouches[0].pageX,
-　　　　moveEndY = e.changedTouches[0].pageY,
-　　　　X = moveEndX - startX,
-　　　　Y = moveEndY - startY;
-　　});
-    wrapper.addEventListener("touchend", function() {
-        if (Math.abs(X) > Math.abs(Y) && X > 0 ) {
-            that.changeImage('pre');
-        } else if ( Math.abs(X) > Math.abs(Y) && X < 0 ) {
-            that.changeImage('next');
+        startX = e.changedTouches[0].pageX,
+        startY = e.changedTouches[0].pageY;
+    });
+    wrapper.addEventListener("touchmove", function(e) {
+        moveEndX = e.changedTouches[0].pageX;
+        moveEndY = e.changedTouches[0].pageY;
+        X = moveEndX - lastMoveX;
+        Y = moveEndY - lastMoveY;
+        lastMoveX = moveEndX;
+        lastMoveY = moveEndY;
+        if(that.operate) {
+            var mLeft = parseInt(wrapper.style.marginLeft.split('px')[0]) + Math.floor(X);
+            wrapper.style.marginLeft = mLeft + 'px';
         }
+        X = moveEndX - startX;
+        Y = moveEndY - startY;
+    });
+    wrapper.addEventListener("touchend", function() {
+        setTimeout(() => {
+            if(that.operate) {
+                if (Math.abs(X) > Math.abs(Y) && X > 0) {
+                    // 前一张
+                    Math.abs(X) > (that.container.clientWidth/3)? 
+                        that.changeImage('pre') : that.changeImage();
+                } else if (Math.abs(X) > Math.abs(Y) && X < 0 ) {
+                    // 后一张
+                    Math.abs(X) > (that.container.clientWidth/3)? 
+                        that.changeImage('next') : that.changeImage();
+                }
+            }
+            // 关闭操作
+            that.operate = false;
+        }, 0);
+        
         that.animate();
     });
 }
